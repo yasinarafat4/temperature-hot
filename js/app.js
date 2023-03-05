0bc8c2a82e57211f304665454aad9191992ba6ee
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-03-05 12:01:49 +0600

    city name added

--- js/app.js
+++ js/app.js
@@ -2,15 +2,28 @@
 const API_KEY = `73b0b3bb610d5a2a114acd1c7c7d6414`;
 
 const loadTemperature = (city) => {
+  console.log(city);
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
-
   fetch(url)
     .then((res) => res.json())
     .then((data) => displayTemperature(data));
 };
 const displayTemperature = (data) => {
-  const temperature = document.getElementById("temperature");
   console.log(data.main.temp);
+  const temperature = document.getElementById("temperature");
   temperature.innerText = data.main.temp;
 };
+
+// event listener of search button
+document.getElementById("btn-search").addEventListener("click", function () {
+  const searchField = document.getElementById("search-field");
+  const city = searchField.value;
+  searchField.value = "";
+
+  // set city
+  document.getElementById("city-name").innerText = city;
+
+  loadTemperature(city);
+});
+
 loadTemperature("dhaka");
